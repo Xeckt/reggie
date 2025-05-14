9ed33392b306c907d0a5a87b4341a653dbf244f2
Xeckt <jordansavell1198@gmail.com>
2025-05-14 17:52:07 +0100

    fix values.go funcs to pointer receivers

--- reggie/values.go
+++ reggie/values.go
@@ -1,33 +1,37 @@
 package reggie
 
-import "golang.org/x/sys/windows/registry"
+import (
+	"fmt"
+
+	"golang.org/x/sys/windows/registry"
+)
 
 // GetValue takes a specified registry key and returns the value of the named key `n`.
 // This is a generic wrapper function over registry.GetValue
-func GetValue(k registry.Key, n string) (any, error) {
+func (k *Key) GetValue(n string) (any, error) {
 	var err error
 	var v any
-	_, t, _ := k.GetValue(n, nil)
+	_, t, _ := k.Handle.GetValue(n, nil)
 
 	switch t {
 	case registry.NONE:
 		return nil, nil // Allow nil checks
 
 	case registry.SZ:
-		v, _, err = k.GetStringValue(n)
+		v, _, err = k.Handle.GetStringValue(n)
 
 	case registry.EXPAND_SZ:
-		v, _, err = k.GetStringValue(n)
+		v, _, err = k.Handle.GetStringValue(n)
 		v, err = registry.ExpandString(v.(string))
 
 	case registry.DWORD, registry.QWORD:
-		v, _, err = k.GetIntegerValue(n)
+		v, _, err = k.Handle.GetIntegerValue(n)
 
 	case registry.BINARY:
-		v, _, err = k.GetBinaryValue(n)
+		v, _, err = k.Handle.GetBinaryValue(n)
 
 	case registry.MULTI_SZ:
-		v, _, err = k.GetStringsValue(n)
+		v, _, err = k.Handle.GetStringsValue(n)
 	}
 
 	if err != nil {
@@ -35,4 +39,47 @@ func GetValue(k registry.Key, n string) (any, error) {
 	}
 
 	return v, nil
+}
+
+func (k *Key) CreateValue(key string, value any, valueType uint32) error {
+	var err error
+
+	switch valueType {
+	case registry.SZ:
+		if _, ok := value.(string); !ok {
+			return fmt.Errorf("value is not of type string but of type: %T", value)
+		}
+		err = k.Handle.SetStringValue(key, value.(string))
+	case registry.EXPAND_SZ:
+		if _, ok := value.(string); !ok {
+			return fmt.Errorf("value is not of type string but of type: %T", value)
+		}
+		err = k.Handle.SetExpandStringValue(key, value.(string))
+	case registry.MULTI_SZ:
+		if _, ok := value.(string); !ok {
+			return fmt.Errorf("value is not of type string but of type: %T", value)
+		}
+		err = k.Handle.SetStringsValue(key, value.([]string))
+	case registry.BINARY:
+		if _, ok := value.([]byte); !ok {
+			return fmt.Errorf("value is not of type []byte but of type: %T", value)
+		}
+		err = k.Handle.SetBinaryValue(key, value.([]byte))
+	case registry.QWORD:
+		if _, ok := value.(uint64); !ok {
+			return fmt.Errorf("value is not of type uint64 but of type: %T", value)
+		}
+		err = k.Handle.SetQWordValue(key, value.(uint64))
+	case registry.DWORD:
+		if _, ok := value.(uint32); !ok {
+			return fmt.Errorf("value is not of type uint32 but of type: %T", value)
+		}
+		err = k.Handle.SetDWordValue(key, value.(uint32))
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
