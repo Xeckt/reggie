976b8552244e196ff0340c89e18f027e15e7d7a4
Xeckt <jordansavell1198@gmail.com>
2025-05-15 10:23:42 +0100

    add basic json export function

---
+++ reggie/json.go
@@ -0,0 +1,42 @@
+package reggie
+
+import "encoding/json"
+
+type keyJson struct {
+	Path    string                 `json:"path"`
+	Subkeys map[string]*subKeyJson `json:"subkeys,omitempty"`
+	Loaded  bool                   `json:"loaded"`
+}
+
+type subKeyJson struct {
+	Name   string         `json:"name"`
+	Values map[string]any `json:"values,omitempty"`
+	Child  *keyJson       `json:"child,omitempty"`
+}
+
+func (k *Key) ExportJson() ([]byte, error) {
+	return json.MarshalIndent(convertKey(k), "", " ")
+}
+
+func convertKey(k *Key) *keyJson {
+	export := &keyJson{
+		Path:   k.Path,
+		Loaded: k.Loaded,
+	}
+
+	if k.Subkeys != nil && len(k.Subkeys) > 0 {
+		export.Subkeys = make(map[string]*subKeyJson)
+		for k, v := range k.Subkeys {
+			data := &subKeyJson{
+				Name:   v.Name,
+				Values: v.Values,
+			}
+			if v.Child != nil {
+				data.Child = convertKey(v.Child)
+			}
+			export.Subkeys[k] = data
+		}
+	}
+
+	return export
+}
